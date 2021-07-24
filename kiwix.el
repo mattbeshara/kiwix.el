71e770ce1e9994cf3e035cbcc7a2f26b1922323c
stardiviner <numbchild@gmail.com>
2021-07-24 20:37:01 +0800

    Update config example in source code document section

--- kiwix.el
+++ kiwix.el
@@ -39,10 +39,14 @@
 ;; (use-package kiwix
 ;;   :ensure t
 ;;   :after org
+;;   :commands (kiwix-launch-server kiwix-at-point kiwix-search-at-library kiwix-search-full-context)
 ;;   :bind (:map document-prefix ("w" . kiwix-at-point))
-;;   :init (setq kiwix-server-type 'docker-local
-;;               kiwix-server-port 8080
-;;               kiwix-default-library "wikipedia_zh_all_2015-11.zim"))
+;;   :custom ((kiwix-server-type 'docker-remote)
+;;            (kiwix-server-url "http://192.168.31.251")
+;;            (kiwix-server-port 8089))
+;;   :hook (org-load . org-kiwix-setup-link)
+;;   :init (require 'org-kiwix)
+;;   :config (add-hook 'org-load-hook #'org-kiwix-setup-link))
 
 ;;;; Usage:
 ;;
